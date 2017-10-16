0155c6651728832df85bc3db320006b7d9d47236
David Glasser <glasser@davidglasser.net>
2017-10-15 22:41:07 -0700

    remap right alt key to cmd (meta)
    alt/option remains on the butterfly

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -139,7 +139,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
+   Key_RightShift, Key_RightGui, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 
 
