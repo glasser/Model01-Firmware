6bbc4435126f3a6f9ee0b54bc8d5184cf0bda273
David Glasser <glasser@davidglasser.net>
2017-12-04 11:32:00 -0800

    All modifiers are left modifiers
    This means I can turn off sticky oneshots with both hands, and they're more
    visually obvious.

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -217,8 +217,8 @@ KEYMAPS(
    M(MACRO_NEXT_RECEIPT),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   OSM(RightAlt),  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   OSM(RightShift), OSM(RightGui), Key_Spacebar, OSM(RightControl),
+   OSM(LeftAlt),  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+   OSM(LeftShift), OSM(LeftGui), Key_Spacebar, OSM(LeftControl),
    ShiftToLayer(FUNCTION)),
 
 #else 
